b3b860c29e89ec86ab552a08cc07ac9ad06f7e8f
nathaniel nosa <nathanielnarth@gmail.com>
2024-02-28 12:50:09 +0100

    User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+//username,password,image,email
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  password: {
+    type: String,
+    required: true
+  },
+
+})
+
+const userModel = mongoose.model('User', userSchema)
+module.exports = userModel
